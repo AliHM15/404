62041e0d600198594c2b40a0c40c22d98c55ff9b
moricetamol <gerhardt.mor@gmail.com>
2025-11-07 21:23:47 +0100

    Rudimentary first page

--- frontend/main.py
+++ frontend/main.py
@@ -1,4 +1,9 @@
 import streamlit as st
-import random
-import time
+
+st.set_page_config(page_title="Demo", page_icon="🤖", layout="centered")
+
+st.title("Minimal Streamlit Page")
+name = st.text_input("Name")
+if name:
+    st.write(f"Hello, {name}!")
 
